1f48a4133432289a2239d54bbe742d79a422aae0
Bruno Quiroga <bquiroga@fi.uba.ar>
2022-09-20 20:45:35 -0300

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.maximize_window()
+
+
+def main():
+    pass
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
